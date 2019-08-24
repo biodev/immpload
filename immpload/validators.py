232d6eb051bd5ea98315c96bf2783a898e1f7b82
FredLoney <loneyf@ohsu.edu>
2019-08-23 21:40:08 -0700

    Split out validation code.

---
+++ immpload/validators.py
@@ -0,0 +1,72 @@
+import functools
+
+
+class ValidationError(Exception):
+    pass
+
+
+def get_validator(template):
+    """
+    Returns the callback function to validate output rows.
+
+    The callback function takes two parameters:
+    * `out_col_ndx_map` - the output column {name: index} dictionary
+    * `row` - the output row
+
+    The callback function modifies the given row in place. It does
+    not return a value.
+
+    *Note*: although the callback raises an exception if certain
+    values necessary to make a default are missing, it does not
+    fully validate the output row. The :mod:`validators` module
+    serves that purpose.
+    :param template: the template name
+    :return: the validator, if any
+    """
+    required = _get_required(template)
+    if required:
+        return functools.partial(_validate, required)
+
+
+def _get_required(template):
+    """
+    :param template: the template name
+    :return: the required columns, if any
+    """
+    required = REQUIRED_COLUMNS.get(template)
+    if required:
+        return required
+    elif '.' in template:
+        prefix = template.split('.')[0]
+        return REQUIRED_COLUMNS.get(prefix)
+
+
+def _validate(required, row, out_col_ndx_map):
+    missing = [col for col in required
+               if row[out_col_ndx_map[col]] == None]
+    if missing:
+        raise ValidationError("Missing values for columns: %s" % missing)
+
+
+"""The validation functions."""
+REQUIRED_COLUMNS = dict(
+    subjectAnimals=['Subject ID', 'Arm Or Cohort ID', 'Gender',
+                    'Min Subject Age', 'Age Unit', 'Age Event',
+                    'Subject Location', 'Species', 'Strain'],
+    subjectHumans=['Subject ID', 'Arm Or Cohort ID', 'Gender',
+                   'Min Subject Age', 'Age Unit', 'Age Event',
+                   'Subject Location', 'Ethnicity', 'Race'],
+    protocols=['User Defined ID', 'File Name', 'Name'],
+    experimentSamples=['Study ID', 'Expsample ID', 'Reagent ID(s)',
+                       'Treatment ID(s)', 'Biosample ID', 'Subject ID',
+                       'Planned Visit ID', 'Study Time Collected',
+                       'Study Time Collected Unit', 'Study Time T0 Event',
+                       'Protocol ID(s)', 'Experiment ID', 'Experiment Name',
+                       'Measurement Technique','Analyte Reported',
+                       'Value Reported', 'Unit Reported'],
+    treatments=['User Defined ID', 'Name', 'Use Treatment?'],
+    reagents=['User Defined ID', 'Manufacturer', 'Catalog Number',
+              'Analyte Reported'],
+    assessments=['Subject ID', 'Assessment Panel ID', 'Study ID',
+                 'Panel Name Reported', 'Component Name Reported',
+                 'User Defined ID', 'Planned Visit ID', 'Study Day'])
